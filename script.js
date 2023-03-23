ce9ec9f5bd69655429130af58616843119f2d292
PravinElangovan <115397005+PravinElangovan@users.noreply.github.com>
2023-03-23 16:49:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // complete the given function
 
-function palindrome(str){
+function isPalindrome(s) {
+  // Convert the string to lowercase and remove non-alphanumeric characters
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
+  // Compare the original string to its reversed version
+  return s === s.split('').reverse().join('');
 }
 module.exports = palindrome
